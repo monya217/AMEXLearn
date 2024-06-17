de59ca6e573cd2e8006a61e51e10f38dab73bb7f
monya217 <monyamehta2107@gmail.com>
2024-06-17 17:14:07 +0530

    adding edit and delete functionality from blog page

--- src/pages/contribute/Blogpage.jsx
+++ src/pages/contribute/Blogpage.jsx
@@ -1,13 +1,20 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { doc, getDoc } from 'firebase/firestore';
-import { firestore } from '../../firebase/firebase';
-import { Box, Text, Heading, Avatar, Flex, Container, Grid, GridItem } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import { doc, getDoc, deleteDoc } from 'firebase/firestore';
+import { firestore, auth } from '../../firebase/firebase';
+import { Box, Text, Heading, Avatar, Flex, IconButton, useToast ,Stack} from '@chakra-ui/react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 import ContributeSidebar from '../../components/Contribute/ContributeSidebar';
+import Spinner from '../../components/Contribute/Spinner';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Blogpage = () => {
     const { id } = useParams();
     const [blog, setBlog] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const toast = useToast();
+    const [user] = useAuthState(auth); // Assuming 'auth' is your Firebase authentication instance
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (id) {
@@ -20,66 +27,105 @@ const Blogpage = () => {
         const docRef = doc(firestore, 'blogs', id);
         const blogDetail = await getDoc(docRef);
         setBlog(blogDetail.data());
+        setLoading(false);
+    };
+
+    const handleDelete = async () => {
+        if (window.confirm("Are you sure you want to delete this blog?")) {
+            try {
+                setLoading(true);
+                await deleteDoc(doc(firestore, "blogs", id));
+                toast({
+                    title: "Blog deleted.",
+                    description: "The blog has been deleted successfully.",
+                    status: "success",
+                    duration: 5000,
+                    isClosable: true,
+                });
+                setLoading(false);
+                navigate('/contribute'); 
+            } catch (err) {
+                console.error(err);
+                toast({
+                    title: "Error deleting blog.",
+                    description: "An error occurred while deleting the blog.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
+                setLoading(false);
+            }
+        }
     };
 
     return (
         <Flex minH="100vh" width="100%">
             <ContributeSidebar />
             <Box flex="1" pl={0} pt="50px">
-                <Box position="relative" mb="4" height="400px" overflow="hidden">
-                    <Box
-                        bgImage={`url('${blog?.imgUrl}')`}
-                        bgSize="cover"
-                        bgPosition="center"
-                        height="100%"
-                    >
-                        <Box
-                            position="absolute"
-                            top="0"
-                            left="0"
-                            width="100%"
-                            height="100%"
-                            bg="rgba(0,0,0,0.5)"
-                        />
-                    </Box>
-                    <Flex
-                        direction="column"
-                        align="center"
-                        justify="center"
-                        height="100%"
-                        color="white"
-                        position="absolute"
-                        top="0"
-                        left="0"
-                        width="100%"
-                        padding="4"
-                        textAlign="center"
-                    >
-                        <Heading as="h1" size="2xl">{blog?.title}</Heading>
-                    </Flex>
-                </Box>
-                <Container maxW="100%" py="6">
-                    <Flex minH="calc(100vh - 400px)" direction="column" align="center" justify="center" width="100%">
-                        <Grid templateColumns="repeat(12, 1fr)" gap={6} width="100%">
-                            <GridItem colSpan={{ base: 12, md: 12 }} mx="auto">
-                                <Box width="100%" maxW="1000px" mx="auto" textAlign="left">
-                                    <Flex align="center" mb="6">
-                                        <Avatar name={blog?.author} size="md" mr="4" />
-                                        <Box>
-                                            <Text fontWeight="bold" fontSize="lg">{blog?.author}</Text>
-                                            <Text color="gray.600">
-                                                {blog?.Timestamp?.toDate().toDateString()}
-                                            </Text>
-                                        </Box>
+                {loading ? (
+                    <Spinner />
+                ) : (
+                    <Box>
+                        <Box position="relative" mb="4" height="400px" overflow="hidden">
+                            <Box
+                                bgImage={`url('${blog?.imgUrl}')`}
+                                bgSize="cover"
+                                bgPosition="center"
+                                height="100%"
+                            >
+                                <Box
+                                    position="absolute"
+                                    top="0"
+                                    left="0"
+                                    width="100%"
+                                    height="100%"
+                                    bg="rgba(0,0,0,0.5)"
+                                />
+                            </Box>
+                            <Flex
+                                direction="column"
+                                align="center"
+                                justify="center"
+                                height="100%"
+                                color="white"
+                                position="absolute"
+                                top="0"
+                                left="0"
+                                width="100%"
+                                padding="4"
+                                textAlign="center"
+                            >
+                                <Heading as="h1" size="2xl">{blog?.title}</Heading>
+                            </Flex>
+                        </Box>
+                        <Box mx="auto" maxW="1000px" textAlign="left" px={{ base: 4, md: 6, lg: 8, xl: 10 }}>
+                            <Stack spacing="4">
+                                <Flex align="center" justify="space-between">
+                                    <Flex align="center">
+                                        <Avatar name={blog?.author} size="md" mr="2" />
+                                        <Stack spacing="1">
+                                            <Text fontSize="md" color="gray.600">{blog?.author}</Text>
+                                            <Text fontSize="sm" color="gray.600">{new Date(blog?.Timestamp?.seconds * 1000).toLocaleDateString()}</Text>
+                                        </Stack>
                                     </Flex>
-                                    <Text fontSize="lg" lineHeight="tall" whiteSpace="pre-wrap" textAlign="left" width="100%" >
-                                        {blog?.description}
-                                    </Text>
-                                </Box>
-                            </GridItem>
-                        </Grid>
-                    </Flex>
-                </Container>
+                                    {(user && blog && user.uid === blog.userId) && (
+                                        <Flex alignItems="center">
+                                            <Link to={`/contribute/post?blogId=${id}`}>
+                                                <IconButton aria-label="Edit" icon={<FaEdit />} size="md" />
+                                            </Link>
+                                            <Box ml="2">
+                                                <IconButton aria-label="Delete" icon={<FaTrash />} size="md" onClick={handleDelete} />
+                                            </Box>
+                                        </Flex>
+                                    )}
+                                </Flex>
+                                <Text fontSize="lg" lineHeight="tall" whiteSpace="pre-wrap" textAlign="left" width="100%" >
+                                    {blog?.description}
+                                </Text>
+                            </Stack>
+                        </Box>
+                    </Box>
+                )}
             </Box>
         </Flex>
     );
