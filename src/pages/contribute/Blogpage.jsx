92b5e9df4bfd168e51ccacb2e43c5775a88904e3
dubeyayushi <adayushi232@gmail.com>
2024-06-17 11:52:17 +0530

    update Blogpage.jsx

--- src/pages/contribute/Blogpage.jsx
+++ src/pages/contribute/Blogpage.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../../firebase/firebase';
 import { Box, Text, Heading, Avatar, Flex, Container, Grid, GridItem } from '@chakra-ui/react';
-import ContributeSidebar from '../../components/Contribute/ContributeSidebar'; // Adjust the import path as needed
+import ContributeSidebar from '../../components/Contribute/ContributeSidebar';
 
 const Blogpage = () => {
     const { id } = useParams();
@@ -23,63 +23,63 @@ const Blogpage = () => {
     };
 
     return (
-        <Box paddingTop="50px">
-            <Box position="relative" mb="4" height="400px" overflow="hidden">
-                <Box
-                    bgImage={`url('${blog?.imgUrl}')`}
-                    bgSize="cover"
-                    bgPosition="center"
-                    height="100%"
-                >
+        <Flex minH="100vh">
+            <ContributeSidebar />
+            <Box flex="1" pl={0} pt="50px">
+                <Box position="relative" mb="4" height="400px" overflow="hidden">
                     <Box
+                        bgImage={`url('${blog?.imgUrl}')`}
+                        bgSize="cover"
+                        bgPosition="center"
+                        height="100%"
+                    >
+                        <Box
+                            position="absolute"
+                            top="0"
+                            left="0"
+                            width="100%"
+                            height="100%"
+                            bg="rgba(0,0,0,0.5)"
+                        />
+                    </Box>
+                    <Flex
+                        direction="column"
+                        align="center"
+                        justify="center"
+                        height="100%"
+                        color="white"
                         position="absolute"
                         top="0"
                         left="0"
                         width="100%"
-                        height="100%"
-                        bg="rgba(0,0,0,0.5)"
-                    />
+                        padding="4"
+                        textAlign="center"
+                    >
+                        <Heading as="h1" size="2xl">{blog?.title}</Heading>
+                    </Flex>
                 </Box>
-                <Flex
-                    direction="column"
-                    align="center"
-                    justify="center"
-                    height="100%"
-                    color="white"
-                    position="absolute"
-                    top="0"
-                    left="0"
-                    width="100%"
-                    padding="4"
-                    textAlign="center"
-                >
-                    <Heading as="h1" size="2xl">{blog?.title}</Heading>
-                </Flex>
-            </Box>
-            <Container maxW="container.xl" py="6"  pl={{ base: "15", md: "10" }}> {/* Adjusted max width to container.xl */}
-                <Grid templateColumns="repeat(12, 1fr)" gap={6}>
-                    <GridItem colSpan={{ base: 12, md: 8 }}>
-                        <Flex direction="column" align="start" pl={{ base: "15", md: "10" }}> {/* Added padding-left */}
-                            <Flex align="center" mb="6">
-                                <Avatar name={blog?.author} size="md" mr="4" />
-                                <Box>
-                                    <Text fontWeight="bold" fontSize="lg">{blog?.author}</Text>
-                                    <Text color="gray.600">
-                                        {blog?.Timestamp?.toDate().toDateString()}
-                                    </Text>
-                                </Box>
+                <Container maxW="container.xl" py="6">
+                    <Grid templateColumns="repeat(12, 1fr)" gap={6}>
+                        <GridItem colSpan={{ base: 12, md: 8 }}>
+                            <Flex direction="column" align="start">
+                                <Flex align="center" mb="6">
+                                    <Avatar name={blog?.author} size="md" mr="4" />
+                                    <Box>
+                                        <Text fontWeight="bold" fontSize="lg">{blog?.author}</Text>
+                                        <Text color="gray.600">
+                                            {blog?.Timestamp?.toDate().toDateString()}
+                                        </Text>
+                                    </Box>
+                                </Flex>
+                                <Text fontSize="lg" lineHeight="tall" whiteSpace="pre-wrap"> {/* Ensuring line breaks are maintained */}
+                                    {blog?.description}
+                                </Text>
                             </Flex>
-                            <Text fontSize="lg" lineHeight="tall" whiteSpace="pre-wrap"> {/* Ensuring line breaks are maintained */}
-                                {blog?.description}
-                            </Text>
-                        </Flex>
-                    </GridItem>
-                    <GridItem colSpan={{ base: 12, md: 4 }}>
-                        <ContributeSidebar />
-                    </GridItem>
-                </Grid>
-            </Container>
-        </Box>
+                        </GridItem>
+                    </Grid>
+                </Container>
+            </Box>
+        </Flex>
     );
 };
 
