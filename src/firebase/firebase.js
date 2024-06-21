7b2718c4481c5649904e5484177f9a31de9be180
dubeyayushi <adayushi232@gmail.com>
2024-06-21 11:07:45 +0530

    add env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,32 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMNJDGUSfcgD6MfbFF_1mNR3q83evUbrM",
-  authDomain: "amexlearn-f477b.firebaseapp.com",
-  projectId: "amexlearn-f477b",
-  storageBucket: "amexlearn-f477b.appspot.com",
-  messagingSenderId: "545469997332",
-  appId: "1:545469997332:web:411575e6e57c5ebd216201",
-  measurementId: "G-KLQM4Q26ZK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
+// Log the config to ensure variables are read correctly
+console.log('Firebase config:', firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Check if the app is initialized correctly
+if (!app) {
+  throw new Error("Firebase app initialization failed");
+}
+
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
-export {app, auth, firestore, storage};
+export { app, auth, firestore, storage };
